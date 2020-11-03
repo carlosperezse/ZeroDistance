b5592a95019e0aa7ba305c500bbff8131e0a31cb
carlosperezse <73508867+carlosperezse@users.noreply.github.com>
2020-11-02 22:09:49 -0600

    Add files via upload
    Primeras pantallas del proyecto

---
+++ ZeroDistance/Login/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/a399b7469e.js" crossorigin="anonymous"></script>
+    <title>Login</title>
+</head>
+<body>
+    <div class="content">
+    
+        <section class="children one">
+            <section class="item-title">
+                <h1>Sing in to ZeroDistance</h1>
+            </section>
+            <section class="item-boxes">
+                <input type="email" name="" id="" placeholder="    &#xf0e0; Email">
+                <input type="password" name="" id="" placeholder="    &#xf3ed; Password">
+            </section>
+            <section class="item-forgot">
+                <a href="">Forgot your password?</a>
+            </section>
+            <section class="item-button">
+                <input type="submit" value="SIGN IN">
+            </section>
+        </section>
+        <section class="children two">
+
+            <section class="item-title-create"> 
+                <h1>Hello</h1>
+            </section>
+            <section class="item-paragraph">
+                <p>To check the health status<br>of a patient, please enter<br>your code.</p>
+            </section>
+            <section class="item-boxes">
+                <input type="text" placeholder="&#xf02a; Enter your code...."/>
+            </section>
+            <section class="item-button-create">
+                <button> SIGN UP </button>
+            </section>
+
+        </section>
+
+    </div>
+</body>
+</html>
---
+++ ZeroDistance/error404/error404.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+      <title>ERROR 404</title>
+      <link rel="stylesheet" href="/error404/public/css/estilos.css">
+  </head>
+  <body>
+    <header>
+        <section id="nombre">
+          <div id="zerodistance">
+            <span>ZeroDistance</span>
+          </div>
+          <div id="slogan">
+            <span>Cuidando de tu familia</span>
+          </div>
+        </section>
+    </header>
+    <section id="muerto">
+        <img id="muerto" src="/error404/public/images/muerto.png" height="140" alt="muerto">
+    </section>
+    <section id="error">
+        <img id="error" src="/error404/public/images/error.png" height="140" alt="error">
+    </section>
+    <section id="salud">
+        <img id="salud" src="/error404/public/images/salud.png" height="190" alt="salud">
+    </section>
+    <section id="pastillas">
+        <img id="pastillas" src="/error404/public/images/pastillas.png" height="150" alt="salud">
+    </section>
+    <div id="aviso">
+      <h1>Error 404</h1>
+      <h2>La p&aacutegina que usted busca no se encuentra disponible.</h2>
+      <h3>Pero no se preocupe, lo mandamos al Inicio:</h3>
+      <a id="volver" href="https://fi.uacam.mx/">Regresar</a>
+    </div>
+  </body>
+</html>
